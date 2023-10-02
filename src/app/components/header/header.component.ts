13fa326046dc91b92b8a75591f3b622a43fd529b
FAVE <jonathan.fave28@gmail.com>
2023-10-02 17:33:36 +0200

    MAJ header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private router: Router) { } // Injectez le Router
+
+  // redirectToHome() {
+  //   this.router.navigate(['/accueil']); // Remplacez '/accueil' par le chemin de votre page d'accueil
+  // }
+  
 }
