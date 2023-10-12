24334d19b673873d543f8abd81d1ecca0356441b
François Vasseur <vasseurfrancois11@gmail.com>
2023-10-12 16:32:04 +0200

    Responsive header and sub-header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Component({
@@ -8,16 +8,27 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   title = 'airbnb';
+  topValue = '13vh';
 
-  //search$: Observable<string> = of('')
   search$: BehaviorSubject<string> = new BehaviorSubject("");
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateTopValue();
+  }
 
   ngOnInit():void{
     this.search$.next("")
+    this.updateTopValue();
   }
 
   public onSearchEmit(search: string): void {
     console.log("search from app : ", search);
     this.search$.next(search);
+  }
+
+  updateTopValue() {
+    const windowHeight = window.innerHeight;
+    this.topValue = windowHeight > 80 ? '80px' : '13vh';
   }
 }
