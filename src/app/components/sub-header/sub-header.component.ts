3a84e17417f4cbe0f98eb1f3af94e8a59abab14d
François Vasseur <vasseurfrancois11@gmail.com>
2023-10-12 12:08:44 +0200

    Add fonts and finish sub-header

--- src/app/components/sub-header/sub-header.component.ts
+++ src/app/components/sub-header/sub-header.component.ts
@@ -1,4 +1,10 @@
-import { Component, HostListener, Renderer2 } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  ElementRef,
+  ViewChild,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-sub-header',
@@ -8,12 +14,56 @@ import { Component, HostListener, Renderer2 } from '@angular/core';
 export class SubHeaderComponent {
   shouldAddShadow: boolean = false;
 
-  scrollImages(direction: string) {
-    // Ajoutez ici la logique pour faire défiler les images
-    console.log('Faire défiler les images vers ' + direction);
-  }
+  @ViewChild('carousel') carousel: ElementRef;
+  @ViewChild('scrollButtonRight') scrollButtonRight: ElementRef;
+  @ViewChild('scrollButtonLeft') scrollButtonLeft: ElementRef;
 
   constructor() {}
+
+  ngAfterViewInit() {
+    const scrollButtonLeft = this.scrollButtonLeft.nativeElement as HTMLElement;
+    scrollButtonLeft.style.display = 'none';
+  }
+
+  scrollImages(direction: string) {
+    const scrollAmount = 600;
+    const currentScrollLeft = this.carousel.nativeElement.scrollLeft;
+    const targetScrollLeft =
+      direction === 'right'
+        ? currentScrollLeft + scrollAmount
+        : currentScrollLeft - scrollAmount;
+
+    this.animateScroll(currentScrollLeft, targetScrollLeft);
+  }
+
+  animateScroll(startScrollLeft: number, targetScrollLeft: number) {
+    const duration = 500; // Durée de l'animation en millisecondes
+    const startTime = performance.now();
+
+    const animate = (currentTime: number) => {
+      const elapsedTime = currentTime - startTime;
+
+      if (elapsedTime < duration) {
+        const scrollProgress = this.easeOutQuad(
+          elapsedTime,
+          startScrollLeft,
+          targetScrollLeft - startScrollLeft,
+          duration
+        );
+        this.carousel.nativeElement.scrollLeft = scrollProgress;
+        requestAnimationFrame(animate);
+      } else {
+        this.carousel.nativeElement.scrollLeft = targetScrollLeft;
+      }
+    };
+
+    requestAnimationFrame(animate);
+  }
+
+  easeOutQuad(t: number, b: number, c: number, d: number) {
+    t /= d;
+    return -c * t * (t - 2) + b;
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll(): void {
@@ -23,5 +73,35 @@ export class SubHeaderComponent {
     const scrollThreshold = 20;
 
     this.shouldAddShadow = scrollPosition > scrollThreshold;
+  }
+
+  handleScroll(event: Event) {
+    const target = event.target as HTMLElement;
+    const scrollButtonRight = this.scrollButtonRight
+      .nativeElement as HTMLElement;
+    const scrollButtonLeft = this.scrollButtonLeft.nativeElement as HTMLElement;
+
+    // Vérifiez si le défilement atteint la fin du carrousel
+    if (target.scrollLeft >= target.scrollWidth - target.clientWidth) {
+      // Cachez le bouton
+      scrollButtonRight.style.display = 'none';
+    } else {
+      // Affichez le bouton
+      scrollButtonRight.style.display = 'inline-block';
+      // Réinitialisez le style du bouton
+      scrollButtonRight.style.position = 'relative';
+      scrollButtonRight.style.transform = 'none';
+      scrollButtonRight.style.display = 'flex';
+    }
+
+    if (target.scrollLeft === 0) {
+      scrollButtonLeft.style.display = 'none';
+    } else {
+      scrollButtonLeft.style.display = 'inline-block';
+      // Réinitialisez le style du bouton
+      scrollButtonLeft.style.position = 'relative';
+      scrollButtonLeft.style.transform = 'none';
+      scrollButtonLeft.style.display = 'flex';
+    }
   }
 }
